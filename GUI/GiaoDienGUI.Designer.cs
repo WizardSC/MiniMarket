9ea5e1914249d3018e1b85d764b08c23dff87380
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-10-03 08:23:01 +0700

    giao diện

---
+++ GUI/GiaoDienGUI.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GUI
+{
+    partial class GiaoDienGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnSidebar = new System.Windows.Forms.Panel();
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.pnHeader = new System.Windows.Forms.Panel();
+            this.pnForm = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnSidebar
+            // 
+            this.pnSidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(254)))), ((int)(((byte)(224)))));
+            this.pnSidebar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnSidebar.Location = new System.Drawing.Point(0, 35);
+            this.pnSidebar.Name = "pnSidebar";
+            this.pnSidebar.Size = new System.Drawing.Size(190, 763);
+            this.pnSidebar.TabIndex = 1;
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(252)))), ((int)(((byte)(237)))));
+            this.pnMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnMenu.Location = new System.Drawing.Point(190, 35);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(1208, 35);
+            this.pnMenu.TabIndex = 2;
+            // 
+            // pnHeader
+            // 
+            this.pnHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(58)))), ((int)(((byte)(191)))), ((int)(((byte)(186)))));
+            this.pnHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnHeader.Name = "pnHeader";
+            this.pnHeader.Size = new System.Drawing.Size(1398, 35);
+            this.pnHeader.TabIndex = 3;
+            // 
+            // pnForm
+            // 
+            this.pnForm.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnForm.Location = new System.Drawing.Point(0, 0);
+            this.pnForm.Name = "pnForm";
+            this.pnForm.Size = new System.Drawing.Size(1398, 798);
+            this.pnForm.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.pnMenu);
+            this.panel3.Controls.Add(this.pnSidebar);
+            this.panel3.Controls.Add(this.pnHeader);
+            this.panel3.Controls.Add(this.pnForm);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1400, 800);
+            this.panel3.TabIndex = 0;
+            // 
+            // GiaoDienGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(252)))), ((int)(((byte)(237)))));
+            this.ClientSize = new System.Drawing.Size(1400, 800);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "GiaoDienGUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GiaoDienGUI";
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnSidebar;
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Panel pnHeader;
+        private System.Windows.Forms.Panel pnForm;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
--- GUI/KhachHangGUI.Designer.cs
+++ GUI/KhachHangGUI.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // KhachHangGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1344, 736);
+            this.Name = "KhachHangGUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "KhachHangGUI";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
