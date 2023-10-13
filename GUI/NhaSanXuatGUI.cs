bf44c97cba2ef0bac0a5f82e572a1ce68a85bccc
HuynhVinh <Chomon0011@gmail.com>
2023-10-13 17:02:36 +0700

    Đã xong GUI

---
+++ GUI/NhaSanXuatGUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class NhaSanXuatGUI : Form
+    {
+        public NhaSanXuatGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
