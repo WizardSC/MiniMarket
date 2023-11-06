d2aab07a607461a8959b14159374938c084c507a
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-11-06 22:35:18 +0700

    hoadon pdf

---
+++ GUI/Reports/HoaDonRP.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace GUI.Reports
+{
+    public partial class HoaDonRP : DevExpress.XtraReports.UI.XtraReport
+    {
+        public HoaDonRP()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
