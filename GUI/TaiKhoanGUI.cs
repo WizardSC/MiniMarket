66eaa8537785b8dd35f6427b969c22f14d503cd4
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-11-11 23:36:28 +0700

    update taikhoan

--- GUI/TaiKhoanGUI.cs
+++ GUI/TaiKhoanGUI.cs
@@ -1,4 +1,5 @@
 ﻿using BLL;
+using DevExpress.Internal.WinApi.Windows.UI.Notifications;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,10 +15,16 @@ namespace GUI
     public partial class TaiKhoanGUI : Form
     {
         private TaiKhoanBLL tkBLL;
+        private NhanVienBLL nvBLL;
+        private DataTable dtNhanVien;
         public TaiKhoanGUI()
         {
             InitializeComponent();
             tkBLL = new TaiKhoanBLL();
+            nvBLL = new NhanVienBLL();
+            dtNhanVien = nvBLL.getListNVHasTaiKhoan();
+            dgvNhanVien.DataSource = dtNhanVien;
+            dtpNgayLap.Value = DateTime.Now;
         }
 
         private void btnRandomPassword_Click(object sender, EventArgs e)
@@ -48,6 +55,13 @@ namespace GUI
             btnThayDoiMK.BackColor = Color.Transparent;
             btnTaoTK.BackColor = Color.Transparent;
 
+        }
+
+        private void dgvNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = dgvNhanVien.CurrentRow.Index;
+            txtNhanVien.Texts = dgvNhanVien.Rows[i].Cells[1].Value.ToString() + dgvNhanVien.Rows[i].Cells[2].Value.ToString();
+            txtQuyen.Texts = dgvNhanVien.Rows[i].Cells[4].Value.ToString();
         }
     }
 }
